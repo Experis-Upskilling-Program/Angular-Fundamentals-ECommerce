3f69b283fc3bc261e53098044771466441d380a2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-09-06 16:41:48 -0500

    [+] info.interface: Define estructura [$] product.interface: Redefine estructura

--- src/app/interfaces/product.ts
+++ src/app/interfaces/product.ts
@@ -1,13 +1,11 @@
+import { Info } from "./info";
+
 export interface Product {
     /** Atributos */
     id: number;
     name: string;
     price?: number;                     // ? Opcional
     description: string | undefined;    // 
-    info: {
-        price?: number,
-        quantity: number,
-        type?: string
-    }
+    info: Info
     /** Metodos (Obvio las interfaces no tienen definicion de metodos, ni funcionalidades dentro de ellos) */
 }
