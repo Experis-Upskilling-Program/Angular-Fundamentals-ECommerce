0e72ec1f9f6bb3ac07ca5c4e004e2e7438d91d6b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-09-06 16:38:21 -0500

    [$] product.interface: Redefine estructura

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'app-products',
@@ -6,26 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-
-  // Encapsular atributos de clase usando accesibilidad
-  title: String = 'Lista de productos';
-  // public title: String = 'Lista de productos';
-  // private title: String = 'Lista de productos';
-  // protected title: String = 'Lista de productos';
-
-  // Tipando variables usando TypeScript
-  value: number;                // Inicialice valor en el contructor
-  value1: number | undefined;   // Soporta dos tipos, inicialmente undefined, por eso no truena.
-  value2!: number;              // Solo soporta un tipo. Yo controlo cuando deja de ser indefinida
-
-  // String[]       -> JavaScript/TypeScript
-  // Array<String>  -> Java
-  fruits: any[] = [ 'Mango', 'Mandarina', 'Melon', 236, true, null, undefined ];
-  frutas: String[] = [ 'Mango', 'Mandarina', 'Melon' ];
-  verduras: Array<String> = [ 'Cebolla', 'Tomate' ];
-  verdureria: ( String | Number ) [] = [ 'Mango', 10, 'Mandarina', 9, 'Melon', 15 ];
-
-  constructor() {
-    this.value = 10;
+  // Implementamos
+  product: Product = {
+    id: 1,
+    name: 'Mango',
+    description: undefined,
+    info: {
+      quantity: 300,
+      type: 'Organico'
+    }
   }
 }
--- src/app/interfaces/product.ts
+++ src/app/interfaces/product.ts
@@ -2,7 +2,12 @@ export interface Product {
     /** Atributos */
     id: number;
     name: string;
-    price: number;
-    description: string;
+    price?: number;                     // ? Opcional
+    description: string | undefined;    // 
+    info: {
+        price?: number,
+        quantity: number,
+        type?: string
+    }
     /** Metodos (Obvio las interfaces no tienen definicion de metodos, ni funcionalidades dentro de ellos) */
 }
