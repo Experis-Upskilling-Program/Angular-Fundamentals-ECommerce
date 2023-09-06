a20646cd44fd563d3cdd3def303b5aab825df6db
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-09-06 15:32:51 -0500

    [+] product.interface: Define estructura

---
+++ src/app/interfaces/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+    /** Atributos */
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    /** Metodos (Obvio las interfaces no tienen definicion de metodos, ni funcionalidades dentro de ellos) */
+}
