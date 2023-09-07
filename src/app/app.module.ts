623f4827898a7a22453262b959a97c67f526e02a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-09-07 15:40:16 -0500

    [+] ng g c components/product-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -8,14 +9,15 @@ import { AppComponent } from './app.component';
 import { SaludoComponent } from './components/saludo/saludo.component';
 import { AdiosComponent } from './components/adios/adios.component';
 import { ProductsComponent } from './components/products/products.component';
-import { FormsModule } from '@angular/forms';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SaludoComponent,
     AdiosComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
