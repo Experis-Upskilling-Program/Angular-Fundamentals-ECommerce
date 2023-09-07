6ac0586d02628b90612e4692e250139b272e96b5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-09-07 15:37:03 -0500

    [+] products.component: Vincular el formulario a la lista a traves de un evento 'click' para llenar los campos del mismo

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -9,11 +9,23 @@ import { PRODUCTS } from '../../../data/fake-products';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  // Implementamos
+  // Atributos de la clase
   product: Product = {
     id: 1,
     name: 'Mora',
     price: 10
   }
   products: Product[] = PRODUCTS;
+  productSelected: Product | undefined;
+
+  constructor() {
+    console.log( this.productSelected );
+  }
+
+  // Metodos de la clase
+  onSelected( product: Product ) {
+    
+    this.productSelected = product;
+    console.log( this.productSelected );
+  }
 }
