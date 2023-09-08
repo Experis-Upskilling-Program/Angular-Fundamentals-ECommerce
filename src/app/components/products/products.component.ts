80d5efa322eaa21fb39da6db3a2362c936e098c2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-09-08 15:10:59 -0500

    [+] products.component: Muestra todos los ciclos de vida del componente e implementa consumo de datos en gnOnInit()

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { Product } from 'src/app/interfaces/product';
 import { PRODUCTS } from '../../../data/fake-products';
 
@@ -8,8 +8,8 @@ import { PRODUCTS } from '../../../data/fake-products';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnChanges {
-  @Input() value: string | undefined;
+export class ProductsComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentInit, AfterViewInit, AfterViewChecked, OnDestroy {
+  @Input() value: string | undefined;   // 'Autoriza' que el componente pueda obtener datos del componente padre
 
   // Atributos de la clase
   product: Product = {
@@ -31,10 +31,50 @@ export class ProductsComponent implements OnChanges {
     console.log( this.productSelected );
   }
 
-  // Ciclos de vida del componente
+  // Ciclos de vida del componente (En desorden, pero ellos se ejecutan en un orden particular)
+  /** 
+   * 1. OnChanges
+   * 2. OnInit
+   * 3. DoCheck
+   * 4. ngAfterContentInit
+   * 5. ngAfterContentChecked
+   * 5. ngAfterViewInit
+   * 6. ngAfterViewChecked
+   * 7. ng
+  */
   ngOnChanges(changes: SimpleChanges): void {
     console.group( 'ngOnChanges' );
     console.log( changes[ 'value' ] );
+    console.groupEnd();
+  }
+  ngOnDestroy(): void {
+    console.group( 'ngOnDestroy' );
+    console.log();
+    console.groupEnd();
+  }
+  ngAfterViewChecked(): void {
+    console.group( 'ngAfterViewChecked' );
+    console.log();
+    console.groupEnd();
+  }
+  ngAfterViewInit(): void {
+    console.group( 'ngAfterViewInit' );
+    console.log();
+    console.groupEnd();
+  }
+  ngAfterContentInit(): void {
+    console.group( 'ngAfterContentInit' );
+    console.log();
+    console.groupEnd();
+  }
+  ngDoCheck(): void {
+    console.group( 'ngDoCheck' );
+    console.log();
+    console.groupEnd();
+  }
+  ngOnInit(): void {
+    console.group( 'ngOnInit' );
+    console.log();
     console.groupEnd();
   }
 }
